0cc514239fbc6bb4baf15679ace8f82b3874db5e
Can Berk İzgi <canberkizgi22@gmail.com>
2019-01-15 10:58:42 +0300

    Optimize resource delete operations

--- src/main/java/com/lbs/re/ui/view/resource/ResourceGridPresenter.java
+++ src/main/java/com/lbs/re/ui/view/resource/ResourceGridPresenter.java
@@ -8,10 +8,13 @@ import org.vaadin.spring.events.EventBus.ViewEventBus;
 
 import com.lbs.re.data.service.REUserService;
 import com.lbs.re.data.service.ResourceService;
+import com.lbs.re.exception.localized.LocalizedException;
 import com.lbs.re.model.ReResource;
+import com.lbs.re.model.ReResourceitem;
 import com.lbs.re.ui.navigation.NavigationManager;
 import com.lbs.re.ui.util.Enums.UIParameter;
 import com.lbs.re.ui.view.AbstractGridPresenter;
+import com.lbs.re.ui.view.resource.edit.ResourceEditPresenter;
 import com.vaadin.spring.annotation.SpringComponent;
 import com.vaadin.spring.annotation.ViewScope;
 
@@ -24,14 +27,30 @@ public class ResourceGridPresenter extends AbstractGridPresenter<ReResource, Res
 	 */
 	private static final long serialVersionUID = 1L;
 
+	private ResourceEditPresenter resourceEditPresenter;
+
 	@Autowired
 	public ResourceGridPresenter(ResourceDataProvider userDataProvider, NavigationManager navigationManager, ResourceService service, BeanFactory beanFactory,
-			ViewEventBus viewEventBus, REUserService userService) {
+			ViewEventBus viewEventBus, REUserService userService, ResourceEditPresenter resourceEditPresenter) {
 		super(navigationManager, service, userDataProvider, beanFactory, viewEventBus, userService);
+		this.resourceEditPresenter = resourceEditPresenter;
 	}
 
 	@Override
 	protected void enterView(Map<UIParameter, Object> parameters) {
+	}
+
+	@Override
+	protected void delete(ReResource item) throws LocalizedException {
+		for (ReResourceitem resourceItem : item.getReResourceitem()) {
+			resourceEditPresenter.deleteLanguagesByItem(resourceItem);
+		}
+		super.delete(item);
+	}
+
+	@Override
+	protected void deleteSelectedLines(Iterable<ReResource> entities) throws LocalizedException {
+		super.deleteSelectedLines(entities);
 	}
 
 }
--- src/main/java/com/lbs/re/ui/view/resource/edit/ResourceEditPresenter.java
+++ src/main/java/com/lbs/re/ui/view/resource/edit/ResourceEditPresenter.java
@@ -14,9 +14,25 @@ import org.vaadin.spring.events.EventBus.ViewEventBus;
 import com.lbs.re.data.service.REUserService;
 import com.lbs.re.data.service.ResourceService;
 import com.lbs.re.data.service.ResourceitemService;
+import com.lbs.re.data.service.impl.language.LanguageServices;
 import com.lbs.re.exception.localized.LocalizedException;
 import com.lbs.re.model.ReResource;
 import com.lbs.re.model.ReResourceitem;
+import com.lbs.re.model.languages.ReAlbaniankv;
+import com.lbs.re.model.languages.ReArabiceg;
+import com.lbs.re.model.languages.ReArabicjo;
+import com.lbs.re.model.languages.ReArabicsa;
+import com.lbs.re.model.languages.ReAzerbaijaniaz;
+import com.lbs.re.model.languages.ReBulgarianbg;
+import com.lbs.re.model.languages.ReEnglishus;
+import com.lbs.re.model.languages.ReFrenchfr;
+import com.lbs.re.model.languages.ReGeorgiange;
+import com.lbs.re.model.languages.ReGermande;
+import com.lbs.re.model.languages.RePersianir;
+import com.lbs.re.model.languages.ReRomanianro;
+import com.lbs.re.model.languages.ReRussianru;
+import com.lbs.re.model.languages.ReTurkishtr;
+import com.lbs.re.model.languages.ReTurkmentm;
 import com.lbs.re.ui.components.grid.REFilterGrid;
 import com.lbs.re.ui.components.grid.RETreeGrid;
 import com.lbs.re.ui.navigation.NavigationManager;
@@ -44,16 +60,18 @@ public class ResourceEditPresenter extends AbstractEditPresenter<ReResource, Res
 	private ResourceItemDataProvider resourceItemDataProvider;
 	private ResourceItemTreeDataProvider resourceItemTreeDataProvider;
 	private ResourceitemService resourceitemService;
+	private LanguageServices languageServices;
 
 	@SuppressWarnings("rawtypes")
 	@Autowired
 	public ResourceEditPresenter(ViewEventBus viewEventBus, NavigationManager navigationManager, ResourceService resourceService, REUserService userService,
 			BeanFactory beanFactory, BCryptPasswordEncoder passwordEncoder, ResourceItemDataProvider resourceItemDataProvider,
-			ResourceItemTreeDataProvider resourceItemTreeDataProvider, ResourceitemService resourceitemService) {
+			ResourceItemTreeDataProvider resourceItemTreeDataProvider, ResourceitemService resourceitemService, LanguageServices languageServices) {
 		super(viewEventBus, navigationManager, resourceService, ReResource.class, beanFactory, userService);
 		this.resourceItemDataProvider = resourceItemDataProvider;
 		this.resourceItemTreeDataProvider = resourceItemTreeDataProvider;
 		this.resourceitemService = resourceitemService;
+		this.languageServices = languageServices;
 	}
 
 	@SuppressWarnings("unchecked")
@@ -111,7 +129,7 @@ public class ResourceEditPresenter extends AbstractEditPresenter<ReResource, Res
 			}
 			treeGrid.getSelectedItems().forEach(resourceItem -> {
 				try {
-					resourceItemDataProvider.deleteLanguagesByItem((ReResourceitem) resourceItem);
+					deleteLanguagesByItem((ReResourceitem) resourceItem);
 					resourceitemService.delete((ReResourceitem) resourceItem);
 				} catch (LocalizedException e) {
 					e.printStackTrace();
@@ -130,7 +148,7 @@ public class ResourceEditPresenter extends AbstractEditPresenter<ReResource, Res
 			}
 			listGrid.getSelectedItems().forEach(resourceItem -> {
 				try {
-					resourceItemDataProvider.deleteLanguagesByItem(resourceItem);
+					deleteLanguagesByItem(resourceItem);
 					resourceitemService.delete(resourceItem);
 				} catch (LocalizedException e) {
 					e.printStackTrace();
@@ -216,6 +234,84 @@ public class ResourceEditPresenter extends AbstractEditPresenter<ReResource, Res
 			getView().getGridResourceItems().getGridDataProvider().refreshDataProviderByItems(itemList);
 		}
 		resourceItemDataProvider.loadTransientData(itemList, getItem().getId());
+	}
+
+	public void deleteLanguagesByItem(ReResourceitem resourceItem) throws LocalizedException {
+
+		ReTurkishtr turkishTr = languageServices.getTurkishService().getLanguageByresourceitemref(resourceItem.getId());
+		if (turkishTr != null) {
+			languageServices.getTurkishService().delete(turkishTr);
+		}
+
+		ReAlbaniankv albanianKv = languageServices.getAlbanianService().getLanguageByresourceitemref(resourceItem.getId());
+		if (albanianKv != null) {
+			languageServices.getAlbanianService().delete(albanianKv);
+		}
+
+		ReArabiceg arabicEg = languageServices.getArabicEgService().getLanguageByresourceitemref(resourceItem.getId());
+		if (arabicEg != null) {
+			languageServices.getArabicEgService().delete(arabicEg);
+		}
+
+		ReArabicjo arabicJo = languageServices.getArabicJoService().getLanguageByresourceitemref(resourceItem.getId());
+		if (arabicJo != null) {
+			languageServices.getArabicJoService().delete(arabicJo);
+		}
+
+		ReArabicsa arabicSa = languageServices.getArabicSaService().getLanguageByresourceitemref(resourceItem.getId());
+		if (arabicSa != null) {
+			languageServices.getArabicSaService().delete(arabicSa);
+		}
+
+		ReAzerbaijaniaz azerbaijaniAz = languageServices.getAzerbaijaniazService().getLanguageByresourceitemref(resourceItem.getId());
+		if (azerbaijaniAz != null) {
+			languageServices.getAzerbaijaniazService().delete(azerbaijaniAz);
+		}
+
+		ReBulgarianbg bulgarianBg = languageServices.getBulgarianService().getLanguageByresourceitemref(resourceItem.getId());
+		if (bulgarianBg != null) {
+			languageServices.getBulgarianService().delete(bulgarianBg);
+		}
+
+		ReEnglishus englishUs = languageServices.getEnglishService().getLanguageByresourceitemref(resourceItem.getId());
+		if (englishUs != null) {
+			languageServices.getEnglishService().delete(englishUs);
+		}
+
+		ReFrenchfr frenchFr = languageServices.getFrenchService().getLanguageByresourceitemref(resourceItem.getId());
+		if (frenchFr != null) {
+			languageServices.getFrenchService().delete(frenchFr);
+		}
+
+		RePersianir persianIr = languageServices.getPersianService().getLanguageByresourceitemref(resourceItem.getId());
+		if (persianIr != null) {
+			languageServices.getPersianService().delete(persianIr);
+		}
+
+		ReGeorgiange georgianGe = languageServices.getGeorgianService().getLanguageByresourceitemref(resourceItem.getId());
+		if (georgianGe != null) {
+			languageServices.getGeorgianService().delete(georgianGe);
+		}
+
+		ReGermande germanDe = languageServices.getGermanService().getLanguageByresourceitemref(resourceItem.getId());
+		if (germanDe != null) {
+			languageServices.getGermanService().delete(germanDe);
+		}
+
+		ReRomanianro romanianRo = languageServices.getRomanianService().getLanguageByresourceitemref(resourceItem.getId());
+		if (romanianRo != null) {
+			languageServices.getRomanianService().delete(romanianRo);
+		}
+
+		ReRussianru russianRu = languageServices.getRussianruService().getLanguageByresourceitemref(resourceItem.getId());
+		if (russianRu != null) {
+			languageServices.getRussianruService().delete(russianRu);
+		}
+
+		ReTurkmentm turkmenTm = languageServices.getTurkmenService().getLanguageByresourceitemref(resourceItem.getId());
+		if (turkmenTm != null) {
+			languageServices.getTurkmenService().delete(turkmenTm);
+		}
 	}
 
 	@Override
